d6e7b822ce9022c448ca2154fd328154d35984cc
saurabhj80 <saurabhj80@gmail.com>
2014-12-13 19:27:47 -0800

    Everything is working great

---
+++ CineInfo/IMDBSearch.swift
@@ -0,0 +1,13 @@
+//
+//  IMDBSearch.swift
+//  CineInfo
+//
+//  Created by Saurabh Jain on 12/13/14.
+//  Copyright (c) 2014 Saurabh jain. All rights reserved.
+//
+
+import UIKit
+
+class IMDBSearch: NSObject {
+   
+}
---
+++ CineInfo/String.swift
@@ -0,0 +1,9 @@
+//
+//  String.swift
+//  CineInfo
+//
+//  Created by Saurabh Jain on 12/13/14.
+//  Copyright (c) 2014 Saurabh jain. All rights reserved.
+//
+
+import Foundation
